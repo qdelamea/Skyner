4af98057a070cf0a90d4b75dab46c183678ff714
Quentin DELAMEA <qdelamea@gmail.com>
2020-04-24 13:16:11 +0200

    Integration of the template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,29 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/more/sidebar-left')
+def sidebarleft():
+    return render_template('sidebar-left.html')
+
+
+@app.route('/more/sidebar-right')
+def sidebarright():
+    return render_template('sidebar-right.html')
+
