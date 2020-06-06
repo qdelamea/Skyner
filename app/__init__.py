ce0d837c012691dd303fac0731f5724fdd71e0ce
Quentin DELAMEA <qdelamea@gmail.com>
2020-06-06 15:36:05 +0200

    Navbar corrections and 404 error handling.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_webglearth import WebGlEarth, WebGl, MAP_TYPES, Marker, Polygon
 
 app = Flask(__name__)
+WebGl(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
-from app import app
+from app import app, WebGl
+from flask_webglearth import WebGlEarth, MAP_TYPES, Marker, Polygon
 
 
 @app.route('/')
@@ -8,9 +9,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/more/project-management')
+def projectManagement():
+    return render_template('project-management.html')
 
 
 @app.route('/contact')
@@ -19,11 +20,35 @@ def contact():
 
 
 @app.route('/more/sidebar-left')
-def sidebarleft():
-    return render_template('sidebar-left.html')
+def technicalAspect():
+    return render_template('technical-aspect.html')
 
 
 @app.route('/more/sidebar-right')
-def sidebarright():
-    return render_template('sidebar-right.html')
+def contribute():
+    return render_template('contribute.html')
 
+
+@app.route('/sign-in')
+def signIn():
+    return render_template('signin.html')
+
+
+@app.route('/sign-up')
+def signUp():
+    return render_template('signup.html')
+
+
+@app.route("/live")
+def live():
+    webgl_earth = WebGlEarth(zoom=1, map_type=MAP_TYPES.get('osm'),
+                             center=[46.3, 30.4], atmosphere=True,
+                             markers=[Marker(49.3, 30.4, "Hello world!")],
+                             polygons=[Polygon(([45.1, 30.3],
+                                                [46.1, 40.56], [50.3, 20.8]))])
+    return render_template('live.html', webgl_map=webgl_earth)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
